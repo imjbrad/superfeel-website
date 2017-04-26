70ccd81ecf17445f1fe3e7d9b09138c2f2425905
Jordan Bradley <jbradley@mica.edu>
2017-04-26 07:16:14 -0400

    init

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+function IndexView(req, res) {
+
+  var context = {
+    title: 'Express',
+    posts: [
+      {
+        image: "images/post-images/oath.jpg",
+        title: "Oath: a poetry performance",
+        type: "Sound"
+      },
+      {
+        image: "images/post-images/automatic.jpg",
+        title: "Automatic",
+        type: "Sound"
+      },
+      {
+        image: "images/post-images/need-that.jpg",
+        title: "Need That",
+        type: "Sound"
+      }
+    ]
+  }
+
+  res.render('index', context);
+}
+
+router.get('/', IndexView);
+module.exports = router;
