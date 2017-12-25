37e7d919e642a8e21e77543961c8552db5247630
Jordan Bradley <jbradley@mica.edu>
2017-12-24 20:55:51 -0500

    improve structured data

--- views/index.js
+++ views/index.js
@@ -6,7 +6,16 @@ function IndexView(req, res) {
   var context = {
     title: 'Superfeel | Official Website of Superfeel',
     description: 'Superfeel is an emerging Brooklyn duo comprised of Jordan Bradley and Androu Boudreau. They’ve been named one of Milk Studio’s 2018 artists to watch',
-    image: 'http://www.isuperfeelyou.com/images/site_image.jpg'
+    image: 'http://www.isuperfeelyou.com/images/site_image.jpg',
+    profiles: {
+      facebook: "https://www.facebook.com/realsuperfeel",
+      instagram: "https://www.instagram.com/realsuperfeel",
+      twitter: "https://www.twitter.com/realsuperfeel",
+      soundcloud: "https://www.soundcloud.com/realsuperfeel",
+      spotify: "https://open.spotify.com/artist/7yrMpBiF2ai2BlWADFKHmU",
+      appleMusic: "https://itunes.apple.com/us/artist/superfeel/1223520586"
+
+    }
   };
 
   res.render('index', context);
